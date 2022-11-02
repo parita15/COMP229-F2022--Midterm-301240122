cb2a04d6a314e44e34b96d580546da889e76ca20
parita15 <parishah052@gmail.com>
2022-11-02 15:21:14 -0400

    changes made

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,7 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://127.0.0.1:27017/midterm"
+  // "URI": "mongodb://127.0.0.1:27017/midterm"
+  // "URI": "mongodb+srv://thomas:Hq3DKYN2NaAqGyyV@mongodbserver.k15hk.azure.mongodb.net/book_store?retryWrites=true&w=majority"
+ "URI": "mongodb+srv://pari:Parita@1502@cluster0.ffexnhk.mongodb.net/midterm?retryWrites=true&w=majority"
+// "URI":"mongodb+srv://cencollege:6GX6OmfUtvvclKT4@cluster0.vikkbvp.mongodb.net/?retryWrites=true&w=majority"
 };
